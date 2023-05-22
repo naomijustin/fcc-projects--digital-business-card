04093738c3f2e6a15db24a7b421cd138cef76931
naomijustin <naomi.z.justin@gmail.com>
2023-05-22 21:27:29 +1000

    Base components created and rendering to DOM

---
+++ src/components/Info.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Info() {
+    return (
+        <h1>Info here</h1>
+    )
+}
+
+export default Info
---
+++ src/components/Interests.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Interests() {
+    return (
+        <h1>Interests here</h1>
+    )
+}
+
+export default Interests
