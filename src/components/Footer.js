03c7e7a1d0c65bfd057a355c026c9c3bc17c1553
naomijustin <naomi.z.justin@gmail.com>
2023-05-29 17:29:36 +1000

    Layout complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from "react"
+import iconTwitter from "../images/TwitterIcon.svg"
+import iconFacebook from "../images/FacebookIcon.svg"
+import iconInstagram from "../images/InstagramIcon.svg"
+import iconGitHub from "../images/GitHubIcon.svg"
 
 function Footer() {
     return (
-        <h1>Footer here</h1>
+        <div className="bg--extra-dark container-width footer--container">
+            <img src={iconTwitter} alt="Twitter icon"></img>
+            <img src={iconFacebook} alt="Facebook icon"></img>
+            <img src={iconInstagram} alt="Instagram icon"></img>
+            <img src={iconGitHub} alt="GitHub icon"></img>
+        </div>
+        
     )
 }
 
