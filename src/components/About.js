e05779191c3f90120915ac60c4a2523cd0f26194
naomijustin <naomi.z.justin@gmail.com>
2023-05-29 16:18:31 +1000

    About Section Done

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,12 @@ import React from "react"
 
 function About() {
     return (
-        <div>
-            <h1>About here</h1>
+        <div className="bg--dark container-width">
+            <div className="paragraph-padding">
+                <h2 className="txt-h2">About</h2>
+                <p>Naomi is an artist, front-end developer, and UX/UI designer based in Sydney, Australia.                     
+                    While she is passionate about art and creativity, she is fascinated in the process of creating experiences that feel smooth and enjoyable for end users.</p>
+            </div>            
         </div>
     )
 }
