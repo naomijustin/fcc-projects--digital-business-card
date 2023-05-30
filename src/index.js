047c8d460a4073edb23fe3f4cbc843c985023d86
naomijustin <naomi.z.justin@gmail.com>
2023-05-30 20:48:56 +1000

    Added Browser Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React, { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from "./App.js"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />               
+        </BrowserRouter>
     </React.StrictMode>
 )
