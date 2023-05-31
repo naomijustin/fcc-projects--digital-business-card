f067992dc04b60b6985569b8fa55d34cede601cb
naomijustin <naomi.z.justin@gmail.com>
2023-05-31 18:07:46 +1000

    added route in Index

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from "./App.js"
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <App />               
-        </BrowserRouter>
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(    
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Route exact path="/" component={App} />               
+    </BrowserRouter>    
 )
