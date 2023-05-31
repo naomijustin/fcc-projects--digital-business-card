c659e83d5ff92ec6f2cf6acb3ec3eb574b8a11ae
naomijustin <naomi.z.justin@gmail.com>
2023-05-31 23:04:07 +1000

    added google console link

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React, { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import App from "./App.js"
 import "./index.css"
-
-// const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: <App />,
-//     },
-//   ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(    
     <React.StrictMode>
         <App />
+        <meta name="google-site-verification" content="gsI3TZirnlQmo8psHvtzmDoFihWWksmKS_d_z2sy4DM" />
     </React.StrictMode>   
 )
